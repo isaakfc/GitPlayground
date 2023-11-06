a3d9f892560b39a760cbca76198139d0adb21021
Isaac Faraz-Chandler <izaacfc@yahoo.co.uk>
2023-11-06 17:15:24 +0000

    New feature added

---
+++ GitPlayground/NewFeature.cpp
@@ -0,0 +1,14 @@
+//
+//  NewFeature.cpp
+//  GitPlayground
+//
+//  Created by Isaac Faraz-Chandler on 06/11/2023.
+//
+
+#include <stdio.h>
+
+
+int newFeature(int number1, int number2)
+{
+    return number1 + number2;
+}
