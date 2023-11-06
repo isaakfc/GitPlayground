703e96bc8ef9fc7148123b2c3c998ac5880a9a2d
Isaac Faraz-Chandler <izaacfc@yahoo.co.uk>
2023-11-06 17:39:16 +0000

    Divergence in branches

--- GitPlayground/main.cpp
+++ GitPlayground/main.cpp
@@ -11,5 +11,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     std::cout << "IM ON FIRE!!!!!" << std::endl;
+    std::cout << "DIVERGENCE!" << std::endl;
     return 0;
 }
