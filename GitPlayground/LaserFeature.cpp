008abbb330de20841807d16551bd94b1014dae1d
Isaac Faraz-Chandler <izaacfc@yahoo.co.uk>
2023-11-06 17:19:13 +0000

    New laser feature added

---
+++ GitPlayground/LaserFeature.cpp
@@ -0,0 +1,13 @@
+//
+//  LaserFeature.cpp
+//  GitPlayground
+//
+//  Created by Isaac Faraz-Chandler on 06/11/2023.
+//
+
+#include <stdio.h>
+
+std::string laserFeature()
+{
+    return "BIG LASER";
+}
