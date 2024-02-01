e06b232854d3d7e02c58a8600c380e4300dacbce
Isaac Faraz-Chandler <izaacfc@yahoo.co.uk>
2024-02-01 15:33:54 +0000

    New feature!

---
+++ GitPlayground/Gun Feature.cpp
@@ -0,0 +1,11 @@
+//
+//  Gun Feature.cpp
+//  GitPlayground
+//
+//  Created by Isaac Faraz-Chandler on 01/02/2024.
+//
+
+#include <stdio.h>
+#include <string>
+
+std::cout << "BIG GUN" << std::endl;
--- GitPlayground/main.cpp
+++ GitPlayground/main.cpp
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     std::cout << "The WORLD IS ENDING!\n";
     std::cout << "IM ON FIRE!!!!!" << std::endl;
     std::cout << "DIVERGENCE!" << std::endl;
+    std::cout << "NEW FEATURE!" << std::endl;
     return 0;
 }
